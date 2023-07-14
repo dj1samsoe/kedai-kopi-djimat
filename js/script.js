b1e14a0eef609519ba8c12c66570e8b1442da872
dj1samsoe <dhanyh86@gmail.com>
2023-07-14 22:22:17 +0700

    Tambahan fitur Search

--- js/script.js
+++ js/script.js
@@ -1,16 +1,30 @@
-// Toggle class active
+// Toggle class active hamburger menu
 const navbarNav = document.querySelector(".navbar-nav");
 
 // Ketika hamburger menu di klik
-document.querySelector("#hamburger-menu").onclick = () => {
+document.querySelector("#hamburger-menu").onclick = (e) => {
   navbarNav.classList.toggle("active");
+  e.preventDefault();
 };
 
-// klik di luar sidebar untuk menghilangkan menu
-const hamburger = document.querySelector("#hamburger-menu");
+// Toggle class active Search form
+const searchForm = document.querySelector(".search-form");
+const searchBox = document.querySelector("#search-box");
+
+document.querySelector("#search-button").onclick = (e) => {
+  searchForm.classList.toggle("active");
+  e.preventDefault();
+};
+
+// klik di luar elemem untuk menghilangkan menu
+const hm = document.querySelector("#hamburger-menu");
+const sb = document.querySelector("#search-button");
 
 document.addEventListener("click", function (e) {
-  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+  if (!hm.contains(e.target) && !navbarNav.contains(e.target)) {
     navbarNav.classList.remove("active");
+  }
+  if (!sb.contains(e.target) && !searchForm.contains(e.target)) {
+    searchForm.classList.remove("active");
   }
 });
